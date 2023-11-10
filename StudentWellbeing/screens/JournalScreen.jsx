17207ca98dadc4e3be0622b971493e0ebe1bb384
Marco Koen <neokocram@gmail.com>
2023-11-10 13:04:06 +1300

    comments

--- StudentWellbeing/screens/JournalScreen.jsx
+++ StudentWellbeing/screens/JournalScreen.jsx
@@ -1,3 +1,14 @@
+/**
+ * Journal 
+ * 
+ * Description:
+ * This screen serves as the main screen for managing journal entries. It allows users to view, create, and edit
+ * journal entries. The entries are displayed in a two-column grid, and each entry includes a title and a truncated
+ * description.
+ * 
+ * @returns {JSX.Element} - Rendered component structure.
+ */
+
 import { View, Text, StyleSheet, TouchableOpacity, Button, FlatList, ScrollView } from "react-native";
 import React, { useState, useEffect } from "react";
 import JournalEntry from "../components/journalModal";
@@ -53,6 +64,7 @@ const Journal = () => {
   const [journal, setJournal] = useState({});
   
 
+   // Fetch journal entries from Firebase on component mount and when modals are closed
     useEffect(() => {
     const fetchJournalData = async () => {
       try{
@@ -70,7 +82,7 @@ const Journal = () => {
       }, [,createJournalVisible, editJournalVisible]);
 
 
-    // Use map to create an array of JSX elements
+    // Use map to create an array of JSX elements for each journal entry
     const Item = ({ item }) => {
       const handleItemPress = (itemId) => {
         setJournal({item})
@@ -79,6 +91,7 @@ const Journal = () => {
     
       return (
         <View style={styles.entries}>
+          {/* Display the JournalUpdate modal if in editing mode */}
           {editJournalVisible ? <JournalUpdate open={editJournalVisible} setOpen={setEditJournalVisible} item={journal}/> : null}
           <TouchableOpacity onPress={() => {handleItemPress(item.id), setEditJournalVisible(true)}}>
             <Text style={styles.heading}>{item.title}</Text>
@@ -92,12 +105,14 @@ const Journal = () => {
 
 return (
   <View style={styles.pageStyle}>
+    {/* Render the "New Entry" button with the JournalEntry modal */}
     {loading ? <Text>Loading....</Text> : 
     <>
     <TouchableOpacity style={[styles.button]}>
         <JournalEntry open={createJournalVisible} setOpen={setCreateJournalVisible}/>
         <Text style={styles.buttonText} onPress={()=>setCreateJournalVisible(true)}>New Entry</Text>
     </TouchableOpacity>
+    {/* ScrollView to display the list of journal entries in a two-column grid */}
     <ScrollView >
       <View>
         <FlatList
